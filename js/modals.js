0f82bc848dc811ee5f3e37016d998b9ee22789f1
David Morilla Cabello <davidmorillacabello@gmail.com>
2023-10-11 15:25:48 +0200

    update

--- js/modals.js
+++ js/modals.js
@@ -23,3 +23,30 @@ span_iros2022.onclick = function() {
   modal_iros2022.style.display = "none";
 }
 
+// Same for modal iros2023
+var modal_iros2023 = document.getElementById("bibtex__modal--iros2023");
+var btn_iros2023 = document.getElementById("bibtex--iros2023");
+var span_iros2023 = document.getElementById("modal__close--iros2023");
+btn_iros2023.onclick = function() {
+  modal_iros2023.style.display = "block";
+}
+window.onclick = function(event) {
+  if (event.target == modal_iros2023) {
+    modal_iros2023.style.display = "none";
+  }
+}
+
+// Same for modal icra2024
+var modal_icra2024 = document.getElementById("bibtex__modal--icra2024");
+var btn_icra2024 = document.getElementById("bibtex--icra2024");
+var span_icra2024 = document.getElementById("modal__close--icra2024");
+btn_icra2024.onclick = function() {
+  modal_icra2024.style.display = "block";
+}
+window.onclick = function(event) {
+  if (event.target == modal_icra2024) {
+    modal_icra2024.style.display = "none";
+  }
+}
+
+
